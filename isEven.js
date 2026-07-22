e9fb12d974560820769914df749d55834090b6f7
tanvir134338 <tanvir134338@gmail.com>
2026-07-23 03:12:59 +0600

    is odd

--- isEven.js
+++ isEven.js
@@ -10,3 +10,5 @@ function isEven(number){
 
 const result= isEven(51);
 console.log(result)
+
+//same jinis ta amra isOdd er khettre mane bijor ber korar jonno o korte pari se khettre amader number%2 === 0 er jaygay 1 dite hbe . ar if return e true ar else return e false dite hbe.
