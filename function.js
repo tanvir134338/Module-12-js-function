734e5406559807f7a127da757581ba35cf48ed23
tanvir134338 <tanvir134338@gmail.com>
2026-07-23 02:04:16 +0600

    recap of previous, parameter, function

--- function.js
+++ function.js
@@ -1,1 +1,26 @@
-gg
+/**
+ * function
+ * give a name for action
+ * give a 1st bracket
+ * after 1st bracket give a 2nd bracket
+ * insie the 2nd bracket
+ * inside the 2nd bracket you will do the work for this function.
+ */
+
+//Function declaration
+
+function fanOnKor() {
+    console.log('walk towards the switch');
+    console.log('press the switch');
+    console.log('come back and do whatever you were going');
+}
+
+function setTheMosqutoNet(){
+    console.log('Unfold the net')
+    console.log('Hook the 4 corner')
+    console.log('Then sing a song mosquito no dinner for you')
+}
+
+//call the function......
+//write the name of the function and give then a 1st bracket.
+fanOnKor()
---
+++ parameter.js
@@ -0,0 +1,28 @@
+//parameter
+
+function sayHello(name){//name ta hosse akta parameter.
+    console.log('Hello!', name, 'vai')
+}
+
+sayHello('Mokbul');
+
+
+function add (num1, num2){
+    console.log('You have provided:', num1, num2)
+    const sum=num1+num2;
+    console.log('Total of those numbers:',sum);
+}
+
+add(365, 978);
+
+
+
+function minus (num3, num4){
+    console.log('You have provided:',num3, num4)
+    const sum = num3 - num4;
+    console.log('Separation of those numbers:', sum);
+}
+
+minus(666,222);
+
+//Aki vabe gun ar vag kora somvob. abar aki vabe element 2 er jaygay aro beshi o neya somvob.kono somossa sara .
